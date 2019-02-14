a6b91356d05096a0c0054455f19db5d06c0cd45c
Alexander Ewetumo <trinoxf@gmail.com>
2019-02-14 15:12:28 +0800

    FIX: Fix expectation test

--- test.sh
+++ test.sh
@@ -3,6 +3,8 @@
 set -e
 echo "" > coverage.txt
 
+export GO111MODULE=on
+
 for d in $(go list ./... | grep -v vendor); do
     go test -race -coverprofile=profile.out -covermode=atomic "$d"
     if [ -f profile.out ]; then
